145aa53e320771b92acb8714440235bc02ad7de9
Srinivasa Vasu <svasu@redhat.com>
2017-04-14 10:42:50 +0530

    Updates [skip ci]

--- zipkin-service/src/main/java/com/redhat/sb/ZipkinServiceApplication.java
+++ zipkin-service/src/main/java/com/redhat/sb/ZipkinServiceApplication.java
@@ -3,13 +3,20 @@ package com.redhat.sb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import zipkin.server.EnableZipkinServer;
 
 @SpringBootApplication
 @EnableZipkinStreamServer
 public class ZipkinServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ZipkinServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinServiceApplication.class, args);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/healthz")
+    public String health() {
+        return "I'm OK";
+    }
 }
